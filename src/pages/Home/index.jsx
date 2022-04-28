db53f4f27e5d0df933d6e01872dcae5b0321c9c7
Rıdvan Akca <akca.ridvann@gmail.com>
2022-04-28 13:59:47 +0300

    filter input is enabled

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import EmptyList from "../../components/common/EmptyList";
 import BlogList from "../../components/Home/BlogList";
 import Header from "../../components/Home/Header";
 import SearchBar from "../../components/Home/SearchBar";
 import { blogList } from "../../config/data";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+
+  // search submit
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) => blog.category.toLowerCase() === searchKey.toLowerCase().trim());
+    setBlogs(filteredBlogs);
+  };
+
+  // clear search
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
   return (
     <div>
       {/* page header */}
       <Header />
       {/* search bar */}
-      <SearchBar />
+      <SearchBar value={searchKey} handleFormSubmit={handleSearchSubmit} handleSearchKey={(e) => setSearchKey(e.target.value)} handleClearSearch={handleClearSearch} />
       {/* blog list */}
-      <BlogList blogs={blogList} />
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </div>
   );
 };
